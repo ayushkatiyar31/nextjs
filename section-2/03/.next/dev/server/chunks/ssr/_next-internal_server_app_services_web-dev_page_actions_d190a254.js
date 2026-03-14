9548288e9f78bab1234d3a5c5f7bf281c95c0314
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2026-03-14 13:31:58 +0530

    dynamic routing

---
+++ section-2/03/.next/dev/server/chunks/ssr/_next-internal_server_app_services_web-dev_page_actions_d190a254.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/services/web-dev/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_services_web-dev_page_actions_d190a254.js.map
---
+++ section-2/03/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/giYHYq22+x/xOntlu5yiLWiNXCKSCtavqtmVBTe22E=\"\n}"
